e37ada85d814b8ddaff9d69764973df176c33d48
3ummerW1nd <sun_0901@foxmail.com>
2021-10-21 10:15:33 +0800

    first commit

--- src/test/java/com/example/jinhecitybussystem/JinheCityBusSystemApplicationTests.java
+++ src/test/java/com/example/jinhecitybussystem/JinheCityBusSystemApplicationTests.java
@@ -26,7 +26,7 @@ class JinheCityBusSystemApplicationTests {
 
     @Test
     void saveAllStation() {
-        String filePath = "/Users/milkdog/IdeaProjects/JinheCityBusSystem/src/main/resources/data/stations.json";
+        String filePath = "src/main/resources/data/stations.json";
         String jsonContent = FileUtil.ReadFile(filePath);
         List<Station> list = JSON.parseArray(jsonContent,Station.class);
         for(Station it : list) {
@@ -36,7 +36,7 @@ class JinheCityBusSystemApplicationTests {
 
     @Test
     void saveAllLine() {
-        String filePath = "/Users/milkdog/IdeaProjects/JinheCityBusSystem/src/main/resources/data/lines.json";
+        String filePath = "src/main/resources/data/lines.json";
         String jsonContent = FileUtil.ReadFile(filePath);
         List<Line> list = JSON.parseArray(jsonContent,Line.class);
         for(Line it : list) {
@@ -45,18 +45,8 @@ class JinheCityBusSystemApplicationTests {
     }
 
     @Test
-    void saveAllTimeTable() {
-        String filePath = "/Users/milkdog/IdeaProjects/JinheCityBusSystem/src/main/resources/data/timetables.json";
-        String jsonContent = FileUtil.ReadFile(filePath);
-        List<TimeTable> list = JSON.parseArray(jsonContent,TimeTable.class);
-        for(TimeTable it : list) {
-            timeTableRepository.save(it);
-        }
-    }
-
-    @Test
     void saveAllRoute() {
-        String filePath = "/Users/milkdog/IdeaProjects/JinheCityBusSystem/src/main/resources/data/routes.json";
+        String filePath = "src/main/resources/data/routes.json";
         String jsonContent = FileUtil.ReadFile(filePath);
         List<Route> list = JSON.parseArray(jsonContent,Route.class);
         for(Route it : list) {
@@ -65,23 +55,5 @@ class JinheCityBusSystemApplicationTests {
                 stationRepository.buildRoute(it.getName(), stations[i], stations[i + 1]);
             }
         }
-//        String filePath = "/Users/milkdog/IdeaProjects/JinheCityBusSystem/src/main/resources/data/routes.json";
-//        String jsonContent = FileUtil.ReadFile(filePath);
-//        List<Route> list = JSON.parseArray(jsonContent,Route.class);
-//        for(Route it : list) {
-//            long[] stations = it.getAlongStation();
-//            for(int i = 0; i < stations.length - 1; i ++) {
-//                Station from = stationRepository.findById(stations[i]);
-//                Station to = stationRepository.findById(stations[i + 1]);
-//                Next temp = nextRepository.save(Next.builder().start(from).end(to).build());
-//                System.out.println(temp);
-//            }
-//        }
     }
-
-    @Test
-    void searchLineByName() {
-        //System.out.println(lineService.findLinesByStationName("锦城广场"));
-    }
-
 }
